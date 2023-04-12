9f349732fcd2400d9ae721962a2e2b0918415643
Julie Achard <julie.achard@heig-vd.ch>
2023-04-12 15:45:07 +0200

    Chargement des données entières

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import { csv } from "d3-fetch";
-import { select } from "d3-selection";
+import { select, selectAll } from "d3-selection";
 import { mean } from "d3-array";
+// Pour importer les données (@rollup/plugin-dsv)
+import planets from "../data/planets.csv";
 
-select("body").append("div").attr("id", "monSvg");
-select("#monSvg").append("svg").attr("width", "500").attr("height", "500");
-
-const dessin = select("svg");
-
-d3.csv("../data/planets.csv").then(function (data) {
+csv("../data/planets.csv").then(function (data) {
   data.map(
     (d) => (
-      (d.Planet = +d.Planet),
+      (d.Planet = d.Planet),
       (d.color = +d.color),
       (d.Mass = +d.Mass),
       (d.Density = +d.Density),
@@ -19,21 +16,5 @@ d3.csv("../data/planets.csv").then(function (data) {
       (d.Distance_from_sun = +d.Distance_from_sun)
     )
   );
-
   console.log(data);
 });
-
-//   dessin
-//     .selectAll("rect")
-//     .data(cleanData)
-//     .join((enter) =>
-//       enter
-//         .append("g")
-//         .append("rect")
-//         .attr("width", "20")
-//         .attr("height", (d) => d.Planet)
-//         .attr("x", (d, i) => i * 30)
-//         .attr("y", (d) => 500 - d * 10)
-
-//Visualiser les données
-//Créer un svg
