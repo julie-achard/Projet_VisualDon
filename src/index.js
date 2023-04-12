a56846f589f9bf4d2e48bb2a779671e50be2fc14
Julie Achard <julie.achard@heig-vd.ch>
2023-04-12 15:02:35 +0200

    d3 test

--- src/index.js
+++ src/index.js
@@ -7,46 +7,33 @@ select("#monSvg").append("svg").attr("width", "500").attr("height", "500");
 
 const dessin = select("svg");
 
-csv("../data/planets.csv")
-  .then(function (data) {
-    data.map(
-      (d) => (
-        (d.Planet = +d.Planet),
-        (d.color = +d.color),
-        (d.Mass = +d.Mass),
-        (d.Density = +d.Density),
-        (d.Diameter = +d.Diameter),
-        (d.Surface_gravity = +d.Surface_gravity),
-        (d.Distance_from_sun = +d.Distance_from_sun)
-      )
-    );
+d3.csv("../data/planets.csv").then(function (data) {
+  data.map(
+    (d) => (
+      (d.Planet = +d.Planet),
+      (d.color = +d.color),
+      (d.Mass = +d.Mass),
+      (d.Density = +d.Density),
+      (d.Diameter = +d.Diameter),
+      (d.Surface_gravity = +d.Surface_gravity),
+      (d.Distance_from_sun = +d.Distance_from_sun)
+    )
+  );
 
-    return data;
-  })
+  console.log(data);
+});
 
-  .then(function (cleanData) {
-    console.log(cleanData);
+//   dessin
+//     .selectAll("rect")
+//     .data(cleanData)
+//     .join((enter) =>
+//       enter
+//         .append("g")
+//         .append("rect")
+//         .attr("width", "20")
+//         .attr("height", (d) => d.Planet)
+//         .attr("x", (d, i) => i * 30)
+//         .attr("y", (d) => 500 - d * 10)
 
-    console.log(
-      `Dans les données, il y'a ${malePercentage * 100}% d'hommes et ${
-        femalePercentage * 100
-      }% de femmes`
-    );
-
-    const planet = cleanData.map((d) => d.Planet);
-
-    console.log(planet);
-
-    //   dessin
-    //     .selectAll("rect")
-    //     .data(cleanData)
-    //     .join((enter) =>
-    //       enter
-    //         .append("g")
-    //         .append("rect")
-    //         .attr("width", "20")
-    //         .attr("height", (d) => d.Planet)
-    //         .attr("x", (d, i) => i * 30)
-    //         .attr("y", (d) => 500 - d * 10)
-    //     );
-  });
+//Visualiser les données
+//Créer un svg
