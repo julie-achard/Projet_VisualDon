399506c2791bba44d35dff486fad803440b017dc
Julie Achard <julie.achard@heig-vd.ch>
2023-04-20 10:00:11 +0200

    Planet + Orbit

--- src/index.js
+++ src/index.js
@@ -16,91 +16,84 @@ import planets from "../data/planets.csv";
 d3.csv("../data/planets.csv").then(function (data) {
   data.map(
     (d) => (
-      (d.Planet = d.Planet),
-      (d.color = +d.color),
+      (d.color = +d.Color),
       (d.Mass = +d.Mass),
       (d.Density = +d.Density),
-      (d.Diameter = d.Diameter),
-      (d.Surface_gravity = +d.Surface_gravity),
-      (d.Distance_from_sun = +d.Distance_from_sun)
+      (d.Diameter = +d["Diameter (km)"]),
+      (d.Distance_from_sun = +d["Distance from Sun (10^6 km)"])
     )
   );
-  console.log(data);
-});
-//Dessin
-const width = 1000;
-const height = 800;
-const svg = d3
-  .select("#container")
-  .append("svg")
-  .attr("width", width)
-  .attr("height", height);
-//Dessin du soleil
-d3.csv("../data/planets.csv").then((data) => {
-  const sun = svg
-    .append("circle")
-    .attr("cx", width / 2)
-    .attr("cy", height / 2)
-    .attr("r", 50)
-    .attr("fill", "yellow");
 
   const planets = data.map((planet) => ({
     name: planet.Planet,
-    distance: +planet.Distance_from_sun * (10 ^ 6),
-    size: +planet.Diameter / 10000,
+    distance: planet.Distance_from_sun * 3,
+    size: planet.Diameter / 300,
     color: planet.Color,
   }));
 
+  //Dessin
+  // Création du SVG pour avoir toutes les planètes
+  const width = 1000;
+  const height = 800;
+  const svg = d3
+    .select("#container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
   //Rectangle descriptif
   var description = d3
-    .select("body")
+    .select("#SunDescription")
     .append("div")
     .attr("id", "description")
-    .style("background-color", "grey")
-    .style("border", "1px solid grey")
+    .style("background-color", "black")
+    .style("border", "1px solid white")
     .style("padding", "10px")
     .style("display", "none")
     .style("position", "absolute")
     .style("left", `${width / 2 - 100}px`) // position horizontale centrée par rapport au cercle du Soleil
     .style("top", `${height / 2 + 70}px`); // position verticale en dessous du cercle du Soleil
 
-  // Ajout d'un événement click au cercle du Soleil pour afficher la fiche descriptive
-  sun.on("click", function () {
-    // Mise à jour du contenu de la fiche descriptive avec les informations du Soleil
-    description.html(
-      "<h2>Description du Soleil</h2><p>Le Soleil est une étoile de type G2 qui se trouve au centre de notre système solaire.</p><p>C'est la source de chaleur et de lumière qui permet la vie sur Terre.</p>"
-    );
-    // Affichage de la fiche descriptive
-    description.style("display", "block");
-  });
-
+  console.log(planets);
   //Pour créer les autres planètes en fonction de leur distance respective mais ça ne fonctionne pas
-  planets.forEach((planet) => {
+  planets.forEach((planet, index) => {
+    // Create orbits center on the sun
     const orbit = svg
-      .append("path")
-      .datum({
-        innerRadius: planet.distance - 10,
-        outerRadius: planet.distance + 10,
-      })
-      .attr(
-        "d",
-        d3
-          .arc()
-          .innerRadius((d) => d.innerRadius)
-          .outerRadius((d) => d.outerRadius)
-          .startAngle(0)
-          .endAngle(2 * Math.PI)
-      )
-      .attr("fill", "none")
-      .attr("stroke", "white")
-      .attr("stroke-width", 1);
-
-    const circle = svg
       .append("circle")
-      .datum(planet)
       .attr("cx", width / 2)
-      .attr("cy", height / 2 - planet.distance)
-      .attr("r", planet.size / 2)
-      .attr("fill", planet.color).call;
+      .attr("cy", height / 2)
+      .attr("r", planet.distance)
+      .attr("fill", "none")
+      .attr("stroke", "grey")
+      .attr("stroke-dasharray", "5,5");
+
+    //Create the sun
+    const soleil = svg
+      .append("circle")
+      .attr("id", "Sun")
+      .attr("cx", width / 2)
+      .attr("cy", height / 2)
+      .attr("r", 20)
+      .attr("fill", "yellow");
+
+    // Ajout d'un événement click au cercle du Soleil pour afficher la fiche descriptive
+    soleil.on("click", function () {
+      // Mise à jour du contenu de la fiche descriptive avec les informations du Soleil
+      description.html(
+        "<h2>Description du Soleil</h2><p>Le Soleil est une étoile de type G2 qui se trouve au centre de notre système solaire.</p><p>C'est la source de chaleur et de lumière qui permet la vie sur Terre.</p>"
+      );
+      // Affichage de la fiche descriptive
+      description.style("display", "block");
+    });
+
+    // Create planets on the orbits
+    svg
+      .append("circle")
+      .attr("id", planet.name)
+      .attr("cx", width / 2 + planet.distance)
+      .attr("cy", height / 2)
+      .attr("r", planet.size) // planet.size for real size
+      .attr("fill", planet.color)
+      .attr("stroke", "white");
   });
 });
