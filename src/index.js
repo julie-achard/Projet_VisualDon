6e680f8f68500224e3f573f3bcf3fcd55ba0f9e0
Julie Achard <julie.achard@heig-vd.ch>
2023-04-21 14:17:55 +0200

    Fiche Soleil + Mercure

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { interpolate } from "d3-interpolate";
 import { format } from "d3-format";
 import { timeParse } from "d3-time-format";
 
+//--TRAITEMENT DES DONNEES--//
+
 // Pour importer les données (@rollup/plugin-dsv)
 import planets from "../data/planets.csv";
 //Trier les données
@@ -27,62 +29,30 @@ csv("../data/planets.csv").then(function (data) {
     size: planet.Diameter / 2000,
     color: planet.Color,
   }));
+  console.log(planets);
 
-  //Dessin
-  // Création du SVG pour avoir toutes les planètes
+  //--- SVG ---//
 
   //La distance entre le soleil et Neptune
   const distanceMax = d3.max(planets.map((d) => d.distance));
-
-  console.log(distanceMax);
-
+  //Taille du svg
   const width = window.innerWidth;
   const height = 800;
-
-  //L'échelle
+  //Echelle
   const scale = d3
     .scaleSqrt()
     .domain([0, distanceMax + 10000])
     .range([200, width]);
-  //Définir le svg à l'échelle de la distance entre le soleil et Neptune
-
+  //Création du svg
   const svg = d3
     .select("#container")
     .append("svg")
     .attr("width", width)
     .attr("height", height);
 
-  //Rectangle descriptif SOLEIL
-  var description = select("#SunDescription1")
-    .append("div")
-    .attr("id", "description")
-    .style("background-color", "black")
-    .style("border", "1px solid white")
-    .style("padding", "10px")
-    .style("display", "none")
-    .style("position", "absolute")
-    .style("left", `${width / 36}px`) // position horizontale centrée par rapport au cercle du Soleil
-    .style("top", `${height / 2 + 80}px`); // position verticale en dessous du cercle du Soleil
-
-  //Fenêtre de droite descriptive qui prend la moitié de la page
-  var description2 = select("#SunDescription2")
-    .append("div")
-    .attr("id", "description")
-    .style("background-color", "black")
-    .style("border", "1px solid white")
-    .style("padding", "200px")
-    .style("display", "none")
-    .style("position", "absolute")
-    .style("left", `${width / 2 + 200}px`) // position horizontale centrée par rapport au cercle du Soleil
-    .style("top", `${height / 14}px`); // position verticale en dessous du cercle du Soleil
-
-  console.log(planets);
-
-  //Pour créer les autres planètes en fonction de leur distance respective mais ça ne fonctionne pas
+  //--CREATION DES PLANETES--//
   planets.forEach((planet, index) => {
-    // // Create orbits of the planets center on the sun
-
-    //Create the sun
+    //Création du soleil
     const soleil = svg
       .append("circle")
       .attr("id", "Sun")
@@ -90,8 +60,7 @@ csv("../data/planets.csv").then(function (data) {
       .attr("cy", height / 2)
       .attr("r", 30)
       .attr("fill", "gold");
-
-    //Faire que les orbites passe par le centre des planètes correspondantes
+    //Création des orbites
     const orbit = svg
       .append("circle")
       .attr("id", "orbit")
@@ -101,64 +70,6 @@ csv("../data/planets.csv").then(function (data) {
       .attr("fill", "none")
       .attr("stroke", "white")
       .attr("stroke-solid", "5,5");
-
-    //Rendre le soleil plus grand au clic
-    soleil.on("click", function () {
-      soleil.transition().duration(1000).attr("r", 100);
-      //Et quelques secondes après afficher la fiche
-      setTimeout(function () {
-        description.html(
-          //Passer  à la ligne le texte
-
-          "<h2>Le Soleil</h2><p> Il est souvent considérée comme le cœur du thème astral.Il représente l'essence de notre être,notre identité et notre volonté de vivre.</p>" +
-            "<p>Il est également considéré comme la source</p>" +
-            "<br>" +
-            "<p>de notre énergie vitale de notre motivation et de notre rayonnement personnel.</p>"
-        );
-
-        //   // Mise à jour du contenu de la fiche descriptive avec les informations du Soleil
-        description.html(
-          "<h2>Le Soleil</h2><p> Il est souvent considérée comme le cœur du thème astral. Il représente l'essence de notre être,</p><p>notre identité et notre volonté de vivre. Il est également considéré comme la source de notre énergie vitale,</p><p>de notre motivation et de notre rayonnement personnel.</p>"
-        );
-        //   // Affichage de la fiche descriptive
-        description.style("display", "block");
-
-        //BOUTTON FERMER
-        description
-          .append("button")
-          .attr("id", "close")
-          .text("Fermer")
-          .on("click", function () {
-            description.style("display", "none");
-            soleil.transition().duration(1000).attr("r", 30);
-          });
-        // BOUTTON SAVOIR PLUS QUI AFFICHE DESCRIPTION 2
-        description
-          .append("button")
-          .attr("id", "close")
-          .text("En savoir plus")
-          .on("click", function () {
-            description2.html(
-              "<h4>Signification</h4><p>L'égo</p>" +
-                "<h4>Symbole</h4><p>Vitalité et virilité</p>" +
-                "<h4>Elément</h4><p>Associé au feu</p>" +
-                "<h4>Couleur</h4><p>Or Jaune</p>"
-            );
-            //FERMER LA DESCRIPTION 2
-            description2.style("display", "block");
-            description2
-              .append("button")
-              .attr("id", "close")
-              .text("Fermer")
-              .on("click", function () {
-                description2.style("display", "none");
-                description.style("display", "none");
-                soleil.transition().duration(1000).attr("r", 30);
-              });
-          });
-      }, 1000);
-    });
-
     // Creation planète
     const boule = svg
       .append("circle")
@@ -168,75 +79,206 @@ csv("../data/planets.csv").then(function (data) {
       .attr("r", planet.size / 2) // planet.size for real size
       .attr("fill", planet.color)
       .attr("stroke", "white");
-    //Faire bouger les planètes légèrement de haut en bas avec une boucle while
-    // boule
-    //   .transition()
-    //   .duration(1000)
-    //   .attr("cy", height / 2 + 10)
-    //   .transition()
-    //   .duration(1000)
-    //   .attr("cy", height / 2 - 10);
+
+    //-- FICHES DESCRIPTIVES SOLEIL --//
+
+    //Description
+    var descriptionSoleil = select("#Description1")
+      .append("div")
+      .attr("id", "description")
+      .style(
+        "background-image",
+        "linear-gradient(to right, khaki, sandybrown )"
+      )
+      .style("border", "none")
+      .style("padding", "10px")
+      .style("display", "none")
+      .style("position", "absolute")
+      .style("left", `${width / 36}px`) // position horizontale centrée par rapport au cercle du Soleil
+      .style("top", `${height / 2 + 183.5}px`); // position verticale en dessous du cercle du Soleil
+
+    //Fiche astrologique
+    var description2Soleil = select("#Description2")
+      .append("div")
+      .attr("id", "description")
+      .style(
+        "background-image",
+        "linear-gradient(to right, sandybrown , khaki)"
+      )
+      .style("border", "none")
+      .style("padding", "200px")
+      .style("display", "none")
+      .style("position", "absolute")
+      .style("left", `${width / 2 + 200}px`) // position horizontale centrée par rapport au cercle du Soleil
+      .style("top", `${height / 14}px`); // position verticale en dessous du cercle du Soleil
+
+    //--INTERACTION SOLEIL & BOUTONS--//
+
+    //agrandir le soleil au clic
+    soleil.on("click", function () {
+      soleil.transition().duration(1000).attr("r", 100);
+      //Affichage de la fiche descriptive
+      setTimeout(function () {
+        descriptionSoleil.html(
+          "<h2>Le Soleil</h2><p> Il est souvent considérée comme le cœur du thème astral.Il représente l'essence de notre être,notre identité et notre volonté de vivre.</p>" +
+            "<p>Il est également considéré comme la source</p>" +
+            "<br>" +
+            "<p>de notre énergie vitale de notre motivation et de notre rayonnement personnel.</p>"
+        );
+        // Mise à jour du contenu de la fiche descriptive avec les informations du Soleil
+        descriptionSoleil.html(
+          "<h2>Le Soleil</h2><p> Il est souvent considérée comme le cœur du thème astral. Il représente l'essence de notre être,</p><p>notre identité et notre volonté de vivre. Il est également considéré comme la source de notre énergie vitale,</p><p>de notre motivation et de notre rayonnement personnel.</p>"
+        );
+        // Affichage de la fiche descriptive
+        descriptionSoleil.style("display", "block");
+
+        //Bouton fermer
+        descriptionSoleil
+          .append("button")
+          .style("border", "1px solid sandybrown")
+          .style("border-radius", "15px")
+          .style("background-color", "white")
+          .style("color", "sandybrown")
+          .attr("id", "close")
+          .text("Fermer")
+          .on("click", function () {
+            descriptionSoleil.style("display", "none");
+            soleil.transition().duration(1000).attr("r", 30);
+          });
+        //Bouton en savoir plus
+        descriptionSoleil
+          .append("button")
+          .style("border", "1px solid sandybrown")
+          .style("border-radius", "15px")
+          .style("background-color", "white")
+          .style("color", "sandybrown")
+          .attr("id", "close")
+          .text("En savoir plus")
+          .on("click", function () {
+            //Affichage de la fiche astrologique
+            description2Soleil.html(
+              "<h4>Signification</h4><p>L'égo</p>" +
+                "<h4>Symbole</h4><p>Vitalité et virilité</p>" +
+                "<h4>Elément</h4><p>Associé au feu</p>" +
+                "<h4>Couleur</h4><p>Or Jaune</p>"
+            );
+            //Bouton fermer fiche astrologique
+            description2Soleil.style("display", "block");
+            description2Soleil
+              .append("button")
+              .attr("id", "close")
+              .style("border", "1px solid sandybrown")
+              .style("border-radius", "15px")
+              .style("background-color", "white")
+              .style("color", "sandybrown")
+              .text("Fermer")
+              .on("click", function () {
+                description2Soleil.style("display", "none");
+                descriptionSoleil.style("display", "none");
+                soleil.transition().duration(1000).attr("r", 30);
+              });
+          });
+      }, 1000);
+      //Fin du premier soleil on click
+    });
+    //Fin de la boucle
   });
 
-  //Faire la fiche descriptive de Mercure
-  const mercure = d3.select("#Mercury");
-  mercure.on("click", function () {
-    description.html(
-      "<h2>Description de Mercure</h2><p>Mercure est la planète la plus proche du Soleil et la moins massive du système solaire.</p><p>Sa température moyenne est de -173°C.</p>"
-    );
-    description.style("display", "block");
-    description
-      .append("button")
-      .attr("id", "close")
-      .text("Fermer")
-      .on("click", function () {
-        description.style("display", "none");
-      });
-  });
+  //--FICHE DESCRIPTIVE MERCURE --//
+  //Description Mercure
+  var descriptionMercure = select("#Description1")
+    .append("div")
+    .attr("id", "description")
+    .style("background-image", "linear-gradient(to right, khaki, sandybrown )")
+    .style("border", "none")
+    .style("padding", "10px")
+    .style("display", "none")
+    .style("position", "absolute")
+    .style("left", `${width / 36}px`) // position horizontale centrée par rapport au cercle du Soleil
+    .style("top", `${height / 2 + 183.5}px`); // position verticale en dessous du cercle du Soleil
 
-  //Fiche descriptive de Vénus
-  const venus = d3.select("#Venus");
-  venus.on("click", function () {
-    description.html(
-      "<h2>Description de Vénus</h2><p>Vénus est la deuxième planète du système solaire par ordre de distance au Soleil.</p><p>Sa température moyenne est de 462°C.</p>"
-    );
-    description.style("display", "block");
-    description
-      .append("button")
-      .attr("id", "close")
-      .text("Fermer")
-      .on("click", function () {
-        description.style("display", "none");
-      });
+  //Fiche astrologique Mercure
+  var description2Mercure = select("#Description2")
+    .append("div")
+    .attr("id", "description")
+    .style("background-image", "linear-gradient(to right, sandybrown , khaki)")
+    .style("border", "none")
+    .style("padding", "200px")
+    .style("display", "none")
+    .style("position", "absolute")
+    .style("left", `${width / 2 + 200}px`) // position horizontale centrée par rapport au cercle du Soleil
+    .style("top", `${height / 14}px`); // position verticale en dessous du cercle du Soleil
+
+  //--INTERACTION MERCURE & BOUTONS--//
+
+  //récupérer la planète Mercure
+  var planetMercure = select("#Mercury");
+
+  planetMercure.on("click", function () {
+    planetMercure.transition().duration(1000).attr("r", 100);
+    //Affichage de la fiche descriptive de Mercure
+    setTimeout(function () {
+      descriptionMercure.html(
+        "<h2>Le Soleil</h2><p> Il est souvent considérée comme le cœur du thème astral.Il représente l'essence de notre être,notre identité et notre volonté de vivre.</p>" +
+          "<p>Il est également considéré comme la source</p>" +
+          "<br>" +
+          "<p>de notre énergie vitale de notre motivation et de notre rayonnement personnel.</p>"
+      );
+      // Mise à jour du contenu de la fiche descriptive avec les informations de Mercure
+      descriptionMercure.html(
+        "<h2>Le Soleil</h2><p> Il est souvent considérée comme le cœur du thème astral. Il représente l'essence de notre être,</p><p>notre identité et notre volonté de vivre. Il est également considéré comme la source de notre énergie vitale,</p><p>de notre motivation et de notre rayonnement personnel.</p>"
+      );
+      // Affichage de la fiche descriptive Mercure
+      descriptionMercure.style("display", "block");
+
+      //Bouton fermer Mercure
+      descriptionMercure
+        .append("button")
+        .style("border", "1px solid sandybrown")
+        .style("border-radius", "15px")
+        .style("background-color", "white")
+        .style("color", "sandybrown")
+        .attr("id", "close")
+        .text("Fermer")
+        .on("click", function () {
+          descriptionMercure.style("display", "none");
+          planet.Mercury.transition().duration(1000).attr("r", 10);
+        });
+      //Bouton en savoir plus Mercure
+      descriptionMercure
+        .append("button")
+        .style("border", "1px solid sandybrown")
+        .style("border-radius", "15px")
+        .style("background-color", "white")
+        .style("color", "sandybrown")
+        .attr("id", "close")
+        .text("En savoir plus")
+        .on("click", function () {
+          //Affichage de la fiche astrologique  Mercure
+          description2Mercure.html(
+            "<h4>Signification</h4><p>L'égo</p>" +
+              "<h4>Symbole</h4><p>Vitalité et virilité</p>" +
+              "<h4>Elément</h4><p>Associé au feu</p>" +
+              "<h4>Couleur</h4><p>Or Jaune</p>"
+          );
+          //Bouton fermer fiche astrologique Mercure
+          description2Mercure.style("display", "block");
+          description2Mercure
+            .append("button")
+            .attr("id", "close")
+            .style("border", "1px solid sandybrown")
+            .style("border-radius", "15px")
+            .style("background-color", "white")
+            .style("color", "sandybrown")
+            .text("Fermer")
+            .on("click", function () {
+              description2Mercure.style("display", "none");
+              descriptionMercure.style("display", "none");
+              planetMercure.transition().duration(1000).attr("r", 10);
+            });
+        });
+    }, 1000);
+    //Fin du premier Mercure on click
   });
-  //Fiche descriptive de la Terre
-  const terre = d3.select("#Earth");
-  terre.on("click", function () {
-    description.html(
-      "<h2>Description de la Terre</h2><p>La Terre est la troisième planète du système solaire par ordre de distance au Soleil.</p><p>Sa température moyenne est de 15°C.</p>"
-    );
-    description.style("display", "block");
-    description
-      .append("button")
-      .attr("id", "close")
-      .text("Fermer")
-      .on("click", function () {
-        description.style("display", "none");
-      });
-  });
-  //Fiche descriptive de Mars
-  const mars = d3.select("#Mars");
-  mars.on("click", function () {
-    description.html(
-      "<h2>Description de Mars</h2><p>Mars est la quatrième planète du système solaire par ordre de distance au Soleil.</p><p>Sa température moyenne est de -65°C.</p>"
-    );
-    description.style("display", "block");
-    description
-      .append("button")
-      .attr("id", "close")
-      .text("Fermer")
-      .on("click", function () {
-        description.style("display", "none");
-      });
-  });
+  //Fin traitement données
 });
